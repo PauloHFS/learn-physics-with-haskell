9cecc6116f087bc5db3a1cc0807fbb7162a73ce8
Paulo Hernane <paulohernane10@gmail.com>
2024-12-04 22:05:01 -0300

    Add additional mathematical functions in exercice2-1.hs

--- cap2/exercice2-1.hs
+++ cap2/exercice2-1.hs
@@ -13,8 +13,28 @@ sinDeg rad = sin $ rad * pi / 180
 cubicrt :: Double -> Double
 cubicrt x = x ** (1 / 3)
 
+e :: Double
 e = exp 1
 
+g :: Double -> Double
 g y = e ** y + 8 ** y
 
+h :: Double -> Double
 h x = 1 / sqrt ((x - 5) ** 2 + 16)
+
+y :: Double -> Double
+y b = 1 / (1 - b ** 2)
+
+u :: Double -> Double
+u x = (1 / (10 + x)) + (1 / (10 - x))
+
+l :: Double -> Double
+l l = sqrt l * (l + 1)
+
+ee :: Double -> Double
+ee x = 1 / abs x ** 3
+
+eee :: Double -> Double
+eee z = 1 / ((z ** 2 + 4) ** (3 / 2))
+
+-- (\b -> 1 / (sqrt (1 - (b ** 2)))) 0.8
